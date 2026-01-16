cdb039e2184e277ba8359274aa930353f3ffb13d
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-01-15 23:11:47 -0400

    Se creó GetEstudianteUseCase.

---
+++ app/src/main/java/edu/ucne/registro_estudiantes/domain/usecase/GetEstudianteUseCase.kt
@@ -0,0 +1,10 @@
+package edu.ucne.registro_estudiantes.domain.usecase
+
+import edu.ucne.registro_estudiantes.domain.model.Estudiante
+import edu.ucne.registro_estudiantes.domain.repository.EstudianteRepository
+
+class GetEstudianteUseCase (
+    private val repository: EstudianteRepository
+) {
+    suspend operator fun invoke(id: Int): Estudiante? = repository.getEstudiante(id)
+}
