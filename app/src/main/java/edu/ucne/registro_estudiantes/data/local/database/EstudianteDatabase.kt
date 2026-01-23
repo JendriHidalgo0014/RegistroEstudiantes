418c0926c05e80a4fc7a9c9213490cb4c3d7bfd3
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-01-23 14:25:25 -0400

    Se agregó la entidad de asignatura en database.

--- app/src/main/java/edu/ucne/registro_estudiantes/data/local/database/EstudianteDatabase.kt
+++ app/src/main/java/edu/ucne/registro_estudiantes/data/local/database/EstudianteDatabase.kt
@@ -2,16 +2,20 @@ package edu.ucne.registro_estudiantes.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.registro_estudiantes.data.local.dao.AsignaturaDao
 import edu.ucne.registro_estudiantes.data.local.dao.EstudianteDao
+import edu.ucne.registro_estudiantes.data.local.entities.AsignaturaEntity
 import edu.ucne.registro_estudiantes.data.local.entities.EstudianteEntity
 
 @Database(
     entities = [
-        EstudianteEntity::class
+        EstudianteEntity::class,
+        AsignaturaEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class EstudianteDatabase : RoomDatabase(){
     abstract fun estudianteDao(): EstudianteDao
+    abstract fun asignaturaDao(): AsignaturaDao
 }
