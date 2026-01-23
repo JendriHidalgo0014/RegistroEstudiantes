0e7e6d32b41604916fdf34277aa780193ade5d14
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-01-23 14:26:27 -0400

    Se creó el modelo de asignatura.

---
+++ app/src/main/java/edu/ucne/registro_estudiantes/domain/model/Asignatura.kt
@@ -0,0 +1,9 @@
+package edu.ucne.registro_estudiantes.domain.model
+
+data class Asignatura(
+    val asignaturaId: Int = 0,
+    val codigo: String,
+    val nombre: String,
+    val aula: String,
+    val creditos: Int
+)
